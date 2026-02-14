7195dacaaa13eb3509b6708b86c23b0c577e6118
ankitchaubey1410 <ankitchaubey1421@gmail.com>
2026-02-14 11:31:05 +0530

    Initial commit

---
+++ simple_face_rect.py
@@ -0,0 +1,55 @@
+import face_recognition
+import cv2
+import os
+import glob
+import numpy as np
+
+class SimpleFaceRec:
+
+    def __init__(self):
+        self.known_face_encodings = []
+        self.known_face_names = []
+
+    def load_encoding_images(self, images_path):
+        images_path = glob.glob(os.path.join(images_path, "*.*"))
+        print(f"{len(images_path)} encoding images found.")
+
+        for img_path in images_path:
+            img = cv2.imread(img_path)
+            rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            basename = os.path.basename(img_path)
+            (filename, ext) = os.path.splitext(basename)
+
+            face_enc = face_recognition.face_encodings(rgb_img)[0]
+
+            self.known_face_encodings.append(face_enc)
+            self.known_face_names.append(filename)
+
+        print("Encoding images loaded")
+
+    def detect_known_faces(self, frame):
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+
+            if matches[best_match_index]:
+                name = self.known_face_names[best_match_index]
+
+            face_names.append(name)
+
+        face_locations = np.array(face_locations)
+        face_locations = face_locations * 4
+
+        return face_locations.astype(int), face_names
+
